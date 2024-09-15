afced64b9f87deb9a5104a9bf82239b83ee4cbc7
➳ᴹᴿ᭄ꌚꁲꋖꁲꋊꁲꌚ ꂡꋪꀤꁏꂡꁲꌚﾂ ᵒᶠᶠᶤᶜᶤᵃˡ <68757679+ShadowIAssasine@users.noreply.github.com>
2024-09-15 15:41:59 +0200

    Update toxi_hub-main.lua

--- src/main/toxi_hub-main.lua
+++ src/main/toxi_hub-main.lua
@@ -39,7 +39,7 @@ local Section = GeneralTab:AddSection({
 })
 
 GeneralTab:AddButton({
-	Name = "Airsoft Battles",
+	Name = "Admin Access",
 	Callback = function()
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/ShadowIAssasine/Toxi-Hub/main/admin_login.lua"))()
       		print("Loading Hub...")
@@ -54,11 +54,11 @@ local GameTab = Window:MakeTab({
 	PremiumOnly = false
 })
 
-local Section = GameTab:AddSection({
+local Section = GameATab:AddSection({
 	Name = "A"
 })
 
-AdminTab:AddButton({
+GameATab:AddButton({
 	Name = "Airsoft Battles",
 	Callback = function()
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/ShadowIAssasine/Toxi-Hub/main/src/game_library/a/airsoft-battles.lua"))()
