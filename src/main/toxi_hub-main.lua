8b289ad8aa227524fdd2ac77f834df7521a58e2f
➳ᴹᴿ᭄ꌚꁲꋖꁲꋊꁲꌚ ꂡꋪꀤꁏꂡꁲꌚﾂ ᵒᶠᶠᶤᶜᶤᵃˡ <68757679+ShadowIAssasine@users.noreply.github.com>
2024-09-15 16:25:19 +0200

    Update toxi_hub-main.lua

--- src/main/toxi_hub-main.lua
+++ src/main/toxi_hub-main.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Toxi Hub // Main Hub 										v0.2.1", HidePremium = false, IntroText = "Toxi Hub | Main Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Toxi Hub // Main Hub 										     v0.2.1", HidePremium = false, IntroText = "Toxi Hub | Main Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
 
 --[[
 Name = <string> - The name of the UI.
